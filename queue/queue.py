e5e29ed5735365954a406140f6a32962f5c4038b
klaus-wq <ks8715@mail.ru>
2024-06-04 00:18:57 +0700

    add queue

---
+++ queue/queue.py
@@ -0,0 +1,18 @@
+class Queue:
+    def __init__(self):
+       self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        return None
+
+    def size(self):
+        return len(self.queue)
+
+
+
+
